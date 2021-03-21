bab3a7d007a469cfc8bc8511c31ed8766e84e0b2
hugh <270222838@qq.com>
2021-03-21 18:32:51 +0800

    deploy

---
+++ precache-manifest.bf77461bf3e35b1e460bb04e57689b3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7111d53bc3af1f41af15",
+    "url": "/easy-account-website/css/app.098110f4.css"
+  },
+  {
+    "revision": "7a2580b9f97d6852617838792a7f3661",
+    "url": "/easy-account-website/index.html"
+  },
+  {
+    "revision": "7111d53bc3af1f41af15",
+    "url": "/easy-account-website/js/app.a9ffd4ec.js"
+  },
+  {
+    "revision": "9522e87de375ab894a0c",
+    "url": "/easy-account-website/js/chunk-vendors.f0588a12.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/easy-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easy-account-website/robots.txt"
+  }
+]);
